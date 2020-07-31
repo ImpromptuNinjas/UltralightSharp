60cc4fb2360e1262994a87a8c9eafceb5d0a8728
Tyler Young <tyler.young@impromptu.ninja>
2020-07-31 17:53:19 -0400

    high level Bitmap bindings
    Former-commit-id: 73ae414c9df265ab85e55488967e2516ae409799

--- UltralightSharp/Bitmap.cs
+++ UltralightSharp/Bitmap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text;
 using InlineIL;
 using JetBrains.Annotations;
@@ -24,6 +25,66 @@ namespace Ultralight {
       var bytes = Encoding.UTF8.GetBytes(path);
       fixed (byte* pBytes = bytes)
         return Ultralight.BitmapWritePng((Bitmap*) p, (sbyte*) pBytes);
+    }
+
+    public static unsafe void Erase(in this Bitmap _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      Ultralight.BitmapErase((Bitmap*) p);
+    }
+
+    public static unsafe uint GetBpp(in this Bitmap _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      return Ultralight.BitmapGetBpp((Bitmap*) p);
+    }
+
+    public static unsafe void* RawPixels(in this Bitmap _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      return Ultralight.BitmapRawPixels((Bitmap*) p);
+    }
+
+    public static unsafe void* LockPixels(in this Bitmap _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      return Ultralight.BitmapLockPixels((Bitmap*) p);
+    }
+
+    public static unsafe void UnlockPixels(in this Bitmap _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      Ultralight.BitmapUnlockPixels((Bitmap*) p);
+    }
+
+    public static unsafe BitmapFormat GetFormat(in this Bitmap _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      return Ultralight.BitmapGetFormat((Bitmap*) p);
+    }
+
+    public static unsafe uint GetHeight(in this Bitmap _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      return Ultralight.BitmapGetHeight((Bitmap*) p);
+    }
+
+    public static unsafe uint GetWidth(in this Bitmap _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      return Ultralight.BitmapGetWidth((Bitmap*) p);
+    }
+
+    public static unsafe UIntPtr GetSize(in this Bitmap _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      return Ultralight.BitmapGetSize((Bitmap*) p);
+    }
+
+    public static unsafe uint GetRowBytes(in this Bitmap _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      return Ultralight.BitmapGetRowBytes((Bitmap*) p);
     }
 
     public static unsafe void SwapRedBlueChannels(in this Bitmap _) {
