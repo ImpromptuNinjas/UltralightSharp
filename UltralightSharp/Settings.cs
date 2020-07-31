10e26b61b2e0a73335042d1f18bef06a8dee3e5d
Tyler Young <tyler.young@impromptu.ninja>
2020-07-31 18:41:57 -0400

    flesh out Settings
    Former-commit-id: 2d9f17f74ad420742c518571e8787b83df55d994

--- UltralightSharp/Settings.cs
+++ UltralightSharp/Settings.cs
@@ -1,9 +1,53 @@
+using InlineIL;
 using JetBrains.Annotations;
 
 namespace Ultralight {
 
   [PublicAPI]
   public readonly ref struct Settings {
+
+    public static unsafe Settings* Create()
+      => AppCore.CreateSettings();
+
+  }
+
+  public static class SettingsExtensions {
+
+    public static unsafe void Destroy(in this Settings _) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      AppCore.DestroySettings((Settings*) p);
+    }
+
+    public static unsafe void SetAppName(this in Settings _, String* name) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      AppCore.SettingsSetAppName((Settings*) p, name);
+    }
+
+    public static unsafe void SetDeveloperName(this in Settings _, String* name) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      AppCore.SettingsSetDeveloperName((Settings*) p, name);
+    }
+
+    public static unsafe void SetFileSystemPath(this in Settings _, String* path) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      AppCore.SettingsSetFileSystemPath((Settings*) p, path);
+    }
+
+    public static unsafe void SetForceCpuRenderer(this in Settings _, bool forceCpu) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      AppCore.SettingsSetForceCpuRenderer((Settings*) p, forceCpu);
+    }
+
+    public static unsafe void SetLoadShadersFromFileSystem(this in Settings _, bool enabled) {
+      IL.Emit.Ldarg_0();
+      IL.Pop(out var p);
+      AppCore.SettingsSetLoadShadersFromFileSystem((Settings*) p, enabled);
+    }
 
   }
 
