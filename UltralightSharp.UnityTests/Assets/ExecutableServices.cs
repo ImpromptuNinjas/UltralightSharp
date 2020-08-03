9137d96fdc61b33ce30a72493f202f0bb51dcd7a
Tyler Young <tyler.young@impromptu.ninja>
2020-08-03 15:57:57 -0400

    [skip ci] add UltralightLibraryProbe method

--- UltralightSharp.UnityTests/Assets/ExecutableServices.cs
+++ UltralightSharp.UnityTests/Assets/ExecutableServices.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Diagnostics;
+using ImpromptuNinjas.UltralightSharp;
+using JetBrains.Annotations;
 using UnityEngine;
 
+[PublicAPI]
 public static class ExecutableServices {
 
   public static void RestoreNuGetPackages() {
@@ -21,6 +24,26 @@ public static class ExecutableServices {
 
     UnityEngine.Debug.LogFormat("Exiting RestoreNuGetPackages");
 
+    //Environment.Exit(Environment.ExitCode);
+    Application.Quit(Environment.ExitCode);
+  }
+
+  public static void UltralightLibraryProbe() {
+    if (Environment.ExitCode != 0)
+      //Environment.Exit(Environment.ExitCode);
+      Application.Quit(Environment.ExitCode);
+
+    UnityEngine.Debug.LogFormat("Entering UltralightLibraryProbe");
+
+    try {
+      UnityEngine.Debug.LogFormat($"Ultralight v{Ultralight.VersionMajor()}.{Ultralight.VersionMinor()}.{Ultralight.VersionPatch()}");
+    }
+    catch (Exception ex) {
+      UnityEngine.Debug.LogFormat($"Error in UltralightLibraryProbe: {ex}");
+      Environment.ExitCode = 1;
+    }
+
+    UnityEngine.Debug.LogFormat("Exiting UltralightLibraryProbe");
     //Environment.Exit(Environment.ExitCode);
     Application.Quit(Environment.ExitCode);
   }
