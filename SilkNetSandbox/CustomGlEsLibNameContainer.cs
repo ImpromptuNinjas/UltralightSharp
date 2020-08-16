f8e797cf742448583d9c9be80b525718c5c7ec0c
Tyler Young <tyler.young@impromptu.ninja>
2020-08-15 20:28:09 -0400

    update paths for osx
    [skip ci][skip unity]

--- SilkNetSandbox/CustomGlEsLibNameContainer.cs
+++ SilkNetSandbox/CustomGlEsLibNameContainer.cs
@@ -1,24 +1,32 @@
+using System;
+using System.IO;
 using Silk.NET.Core.Loader;
 
 /// <summary>Contains the library name of OpenGLES.</summary>
 internal class CustomGlEsLibNameContainer : SearchPathContainer {
 
+  private static readonly string AssemblyPath
+    = new Uri(typeof(CustomGlEsLibNameContainer).Assembly.CodeBase!).LocalPath;
+
+  private static readonly string AssemblyDirectory
+    = Path.GetDirectoryName(AssemblyPath)!;
+  
   /// <inheritdoc />
-  public override string Linux => "libGLESv2.so";
+  public override string Linux { get; } = Path.Combine(AssemblyDirectory, "libGLESv2.so");
 
   /// <inheritdoc />
-  public override string MacOS => "libGLESv2.so";
+  public override string Android { get; } = Path.Combine(AssemblyDirectory, "libGLESv2.so");
 
   /// <inheritdoc />
-  public override string Android => "libGLESv2.so";
+  public override string MacOS { get; } = Path.Combine(AssemblyDirectory, "libGLESv2.dylib");
 
   /// <inheritdoc />
-  public override string IOS => "libGLESv2.so";
+  public override string IOS { get; } = Path.Combine(AssemblyDirectory, "libGLESv2.dylib");
 
   /// <inheritdoc />
-  public override string Windows64 => "libGLESv2.dll";
+  public override string Windows64 { get; } = Path.Combine(AssemblyDirectory, "libGLESv2.dll");
 
   /// <inheritdoc />
-  public override string Windows86 => "libGLESv2.dll";
+  public override string Windows86 { get; } = Path.Combine(AssemblyDirectory, "libGLESv2.dll");
 
 }
